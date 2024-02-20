2d0ea0294312f67084bda6dbb77b691b763878e4
sunil_github <117855897+sunilroutgithub@users.noreply.github.com>
2024-02-20 15:36:35 +0530

    Add files via upload

---
+++ checkPrimeno.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+bool isprime(int n){
+    if(n<=1)
+    return false;
+    for(int i=2;i<n;i++)
+        if(n % i == 0)
+        return false;
+
+    return true;
+    
+}
+int main(){
+    isprime(11) ? cout << " true\n" : cout << "false\n";
+    isprime(19) ? cout << " true\n" : cout << "false\n";
+    return 0;
+}
